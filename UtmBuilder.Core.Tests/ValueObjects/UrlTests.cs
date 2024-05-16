7193c88a367294ebeaa2b18928f2535cf9a506da
Jaqueline Manzano <jaque_manzanos@hotmail.com>
2024-05-16 19:34:24 -0300

    Iniciando implementação dos testes de unidade

---
+++ UtmBuilder.Core.Tests/ValueObjects/UrlTests.cs
@@ -0,0 +1,39 @@
+﻿using UtmBuilder.Core.ValueObjects;
+using UtmBuilder.Core.ValueObjects.Exceptions;
+
+namespace UtmBuilder.Core.Tests.ValueObjects
+{
+    [TestClass]
+    public class UrlTests
+    {
+        [TestMethod("Deve retornar uma exceção quando a url for inválida")]
+        [TestCategory("Testes de URL")]
+        [ExpectedException(typeof(InvalidUrlException))]
+        public void ShouldReturnExceptionWhenUrlIsInvalid()
+        {
+            #region Opção de Test
+            //try
+            //{
+            //    new Url("urlInvalida");
+            //    Assert.Fail();
+            //}
+            //catch (InvalidUrlException)
+            //{
+
+            //    Assert.IsTrue(true);
+            //}
+            #endregion
+
+            new Url("urlInvalida");
+
+        }
+
+        [TestMethod("Não deve retornar uma exceção quando a url for válida")]
+        [TestCategory("Testes de URL")]
+        public void ShouldNotReturnExceptionWhenUrlIsValid()
+        {
+            new Url("https://balta.io");
+            Assert.IsTrue(true);
+        }
+    }
+}
